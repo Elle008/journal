1d6d55e67ab8026050e34d7c7515f15bbaa351e3
Elle08 <maellaineanis@gmail.com>
2022-09-24 10:28:44 +0800

    add partial header and sidebar, created contoller, views and model for category,  completed crud function for category

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,5 +1,59 @@
 class CategoriesController < ApplicationController
 
   def index
+    @categories = Category.all
   end
+
+  def show
+    @categories = Category.all
+    @category = Category.find(params[:id])
+  end
+
+  def new
+    @categories = Category.all
+    @category = Category.new
+  end
+
+  def create
+    @categories = Category.all
+    @category = Category.new(category_params)
+
+
+      if @category.save
+        redirect_to category_url(@category), notice: "Category was successfully created." 
+
+      else
+        render :new, status: :unprocessable_entity 
+
+      end
+  end
+
+  def edit
+    @categories = Category.all
+    @category = Category.find(params[:id])
+  end
+
+
+  def update
+    @category = Category.find(params[:id])
+    if @category.update(category_params)
+      redirect_to category_url(@category), notice: "Category was successfully updated."
+
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @category = Category.find(params[:id])
+    @category.destroy
+    redirect_to categories_url, notice: "Category was successfully destroyed."
+  end
+
+  private
+
+    def category_params
+      params.require(:category).permit(:category_name, :category_description)
+    end
+
 end
